d4482f55b50044087ca8cba3604f0e84d4d9f9c9
yannysjsu <yanheng.ma@sjsu.edu>
2025-05-04 16:34:54 -0700

    ll

--- three-00-starter/js/main.js
+++ three-00-starter/js/main.js
@@ -1,25 +1,64 @@
+
+let scene, camera, cube, renderer, capsule;
+
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+function init(){
+	
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	
+	renderer = new THREE.WebGLRenderer({antialias:true });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animate );
+	document.body.appendChild( renderer.domElement );
+	
+	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	const geometry2 = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
+	const material1 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	const controls = new OrbitControls (camera, renderer.domElement)
+	const loader = new GLTFLoader ();
+	loader.load('assets/dog_shiny.gltf', function (gltf){
+		const dog = gltf.scene;
+		scene.add(dog);
+	})
 
-camera.position.z = 5;
+	const texture = new THREE.TextureLoader().load('textures/grasslight-big.jpg');
+	const material = new THREE.MeshBasicMaterial( { map:texture } );
+	cube = new THREE.Mesh( geometry, material1  );
+	capsule = new THREE.Mesh ( geometry2, material);
+	scene.add( cube );
+	cube.scale.set(2, 2, 4);
+	scene.add( capsule );
+	
+	camera.position.z = 5;
+	
+	
+}
 
 function animate() {
-
+	
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+	capsule.rotation.x += 0.01;
+	capsule.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 
 }
+
+function onWindowReSize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+window.addEventListener('resize', onWindowReSize, false);
+
+init();
+animate();
